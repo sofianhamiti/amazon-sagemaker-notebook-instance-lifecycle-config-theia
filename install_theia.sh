dd5d96311e46ce780699924393f61a331f76ea4d
EC2 Default User <ec2-user@ip-172-16-73-140.eu-west-1.compute.internal>
2021-06-04 12:00:27 +0000

    adjust config paths

---
+++ install_theia.sh
@@ -0,0 +1,51 @@
+set -e
+
+sudo -u ec2-user -i <<'EOP'
+#####################################
+## INSTALL THEIA IDE FROM SOURCE
+#####################################
+EC2_HOME=/home/ec2-user
+mkdir ${EC2_HOME}/theia && cd ${EC2_HOME}/theia
+
+# Install nvm with node and npm.
+curl https://raw.githubusercontent.com/creationix/nvm/v0.37.2/install.sh | bash
+
+source ${EC2_HOME}/.nvm/nvm.sh
+nvm install 12
+nvm use 12
+npm install -g yarn
+
+NODE_OPTIONS=--max_old_space_size=4096
+cp config/package.json -o ${EC2_HOME}/theia/package.json
+nohup yarn &
+
+#####################################
+### CONFIGURE THEIA IDE
+#####################################
+THEIA_PATH=$PATH
+mkdir ${EC2_HOME}/.theia
+cp config/launch.json -o ${EC2_HOME}/.theia/launch.json
+cp config/settings.json -o ${EC2_HOME}/.theia/settings.json
+
+#####################################
+### INTEGRATE THEIA IDE WITH JUPYTER PROXY
+#####################################
+cat >>/home/ec2-user/.jupyter/jupyter_notebook_config.py <<EOC
+c.ServerProxy.servers = {
+  'theia': {
+    'command': ['yarn', '--cwd', '/home/ec2-user/theia', 'start', '/home/ec2-user/SageMaker', '--port', '{port}'],
+    'environment': {'PATH': '${THEIA_PATH}'},
+    'absolute_url': False,
+    'timeout': 30
+  }
+}
+EOC
+
+source /home/ec2-user/anaconda3/bin/activate JupyterSystemEnv
+pip install jupyter-server-proxy pylint autopep8 yapf pyflakes pycodestyle 'python-language-server[all]'
+jupyter labextension install @jupyterlab/server-proxy
+conda deactivate
+EOP
+
+## RESTART THE JUPYTER SERVER
+initctl restart jupyter-server --no-wait
--- on_start.sh
+++ on_start.sh
@@ -4,49 +4,11 @@ sudo -u ec2-user -i <<'EOP'
 #####################################
 ## INSTALL THEIA IDE FROM SOURCE
 #####################################
-EC2_HOME=/home/ec2-user
-mkdir ${EC2_HOME}/theia && cd ${EC2_HOME}/theia
+cd ${HOME}
 
-# Install nvm with node and npm.
-curl https://raw.githubusercontent.com/creationix/nvm/v0.37.2/install.sh | bash
+git clone https://github.com/SofianHamiti/amazon-sagemaker-notebook-instance-lifecycle-config-theia.git
+cd amazon-sagemaker-notebook-instance-lifecycle-config-theia
 
-source ${EC2_HOME}/.nvm/nvm.sh
-nvm install 12
-nvm use 12
-npm install -g yarn
+sh install_theia.sh
 
-NODE_OPTIONS=--max_old_space_size=4096
-curl https://raw.githubusercontent.com/SofianHamiti/amazon-sagemaker-notebook-instance-lifecycle-config-theia/main/config/package.json -o ${EC2_HOME}/theia/package.json
-nohup yarn &
-
-#####################################
-### CONFIGURE THEIA IDE
-#####################################
-THEIA_PATH=$PATH
-mkdir ${EC2_HOME}/.theia
-curl https://raw.githubusercontent.com/SofianHamiti/amazon-sagemaker-notebook-instance-lifecycle-config-theia/main/config/launch.json -o ${EC2_HOME}/.theia/launch.json
-curl https://raw.githubusercontent.com/SofianHamiti/amazon-sagemaker-notebook-instance-lifecycle-config-theia/main/config/settings.json -o ${EC2_HOME}/.theia/settings.json
-
-#####################################
-### INTEGRATE THEIA IDE WITH JUPYTER
-#####################################
-# CONFIGURE JUPYTER PROXY TO MAP TO THE THEIA IDE
-source /home/ec2-user/anaconda3/bin/activate JupyterSystemEnv
-cat >>/home/ec2-user/.jupyter/jupyter_notebook_config.py <<EOC
-c.ServerProxy.servers = {
-  'theia': {
-    'command': ['yarn', '--cwd', '/home/ec2-user/theia', 'start', '/home/ec2-user/SageMaker', '--port', '{port}'],
-    'environment': {'PATH': '${THEIA_PATH}'},
-    'absolute_url': False,
-    'timeout': 30
-  }
-}
-EOC
-
-pip install jupyter-server-proxy pylint autopep8 yapf pyflakes pycodestyle 'python-language-server[all]'
-jupyter labextension install @jupyterlab/server-proxy
-conda deactivate
 EOP
-
-## RESTART THE JUPYTER SERVER
-initctl restart jupyter-server --no-wait
